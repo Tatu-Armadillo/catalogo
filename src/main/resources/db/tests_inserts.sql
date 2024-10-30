615e7d6064e25b9a42bb4b73ec601cd54cdb2cc8
Tatu-Armadillo <prohazor.fa@gmail.com>
2024-10-30 15:57:01 -0300

    basic inserts by product and stock tables

---
+++ src/main/resources/db/tests_inserts.sql
@@ -0,0 +1,23 @@
+INSERT INTO challenge.products (name, price, modelo, fabricante, detalhes) VALUES
+('Produto A', 99.99, 'Modelo A1', 'Fabricante X', 'Detalhes sobre o Produto A'),
+('Produto B', 150.50, 'Modelo B2', 'Fabricante Y', 'Detalhes sobre o Produto B'),
+('Produto C', 75.00, 'Modelo C3', 'Fabricante Z', 'Detalhes sobre o Produto C'),
+('Produto D', 120.00, 'Modelo D4', 'Fabricante X', 'Detalhes sobre o Produto D'),
+('Produto E', 45.30, 'Modelo E5', 'Fabricante Y', 'Detalhes sobre o Produto E'),
+('Produto F', 200.00, 'Modelo F6', 'Fabricante Z', 'Detalhes sobre o Produto F'),
+('Produto G', 60.75, 'Modelo G7', 'Fabricante X', 'Detalhes sobre o Produto G'),
+('Produto H', 89.99, 'Modelo H8', 'Fabricante Y', 'Detalhes sobre o Produto H'),
+('Produto I', 30.00, 'Modelo I9', 'Fabricante Z', 'Detalhes sobre o Produto I'),
+('Produto J', 110.00, 'Modelo J0', 'Fabricante X', 'Detalhes sobre o Produto J');
+
+INSERT INTO challenge.stocks (quantity, last_update, code_number, product) VALUES
+(100, '2024-10-30 10:00:00', 'CODE001', 1),
+(200, '2024-10-30 10:05:00', 'CODE002', 2),
+(50, '2024-10-30 10:10:00', 'CODE003', 3),
+(75, '2024-10-30 10:15:00', 'CODE004', 4),
+(150, '2024-10-30 10:20:00', 'CODE005', 5),
+(25, '2024-10-30 10:25:00', 'CODE006', 6),
+(300, '2024-10-30 10:30:00', 'CODE007', 7),
+(80, '2024-10-30 10:35:00', 'CODE008', 8),
+(60, '2024-10-30 10:40:00', 'CODE009', 9),
+(90, '2024-10-30 10:45:00', 'CODE010', 10);
